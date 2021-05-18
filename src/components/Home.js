e5e9f14217356e243f0608fa6383d9b1f6050164
0xJeremy <jeremy.kanovsky@tufts.edu>
2021-05-18 00:46:38 -0400

    Welcome animation added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,44 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
+import Typewriter from "typewriter-effect";
+
 import Toolbar from "./Toolbar";
+
+const animationTime = "3s";
 
 const useStyles = makeStyles((theme) => ({
   root: {
- 	width: "60%",
- 	textAlign: "center",
- 	margin: '0 auto',
- 	marginTop: '15vh'
+    width: "60%",
+    textAlign: "center",
+    margin: "0 auto",
+    marginTop: "15vh",
   },
   introText: {
-  	fontSize: '4vh',
-  	fontWeight: 600,
-  	width: "100%",
+    fontSize: "4vh",
+    fontWeight: 600,
+    width: "100%",
+    letterSpacing: "12px",
   },
   welcome: {
-  	float: "left",
+    // textAlign: "left",
+    animation: `$fadeInDown ${animationTime}`,
   },
   to: {
-  	textAlign: "center",
+    textAlign: "center",
+    animation: `$fadeInUp ${animationTime}`,
   },
   the: {
-  	textAlign: "right",
+    textAlign: "right",
+    animation: `$fadeInDown ${animationTime}`,
   },
   titleText: {
-  	fontSize: "clamp(5rem, 14vw, 14rem)",
+    fontSize: "clamp(5rem, 14vw, 14rem)",
     textAlign: "center",
     fontWeight: 600,
-    letterSpacing: '-2px',
+    letterSpacing: "-2px",
     fontFamily: "Cousine",
-    lineHeight: '0.95em',
+    lineHeight: "0.95em",
   },
   daydream: {
     willChange: "auto",
@@ -38,11 +46,17 @@ const useStyles = makeStyles((theme) => ({
     backgroundRepeat: "no-repeat",
     // gradient from https://products.ls.graphics/mesh-gradients/
     backgroundImage: "url(\"https://products.ls.graphics/mesh-gradients/images/05.-Flax.jpg\")",
+    // backgroundImage: 'url("static/Ronchi.jpg")',
     backgroundSize: "200%",
     backgroundClip: "text",
     WebkitBackgroundClip: "text",
     color: "transparent",
     animation: "$bg 8s cubic-bezier(0.77, 0, 0.175, 1) 2s infinite",
+  },
+  tagline: {
+    fontWeight: 600,
+    marginTop: "10vh",
+    fontSize: "4vh",
   },
   "@keyframes bg": {
     "0%": {
@@ -63,30 +77,51 @@ const useStyles = makeStyles((theme) => ({
       backgroundPosition: "5% 5%",
     },
   },
+  "@keyframes fadeInDown": {
+    "0%": {
+      opacity: "0",
+      transform: "translateY(-20px)",
+    },
+    "100%": {
+      opacity: "1",
+      transform: "translateY(0)",
+    },
+  },
+  "@keyframes fadeInUp": {
+    "0%": {
+      opacity: "0",
+      transform: "translateY(20px)",
+    },
+    "100%": {
+      opacity: "1",
+      transform: "translateY(0)",
+    },
+  },
 }));
 
 export default function Home() {
-	const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <div>
       <Toolbar />
       <div className={classes.root}>
-	      <Grid container spacing={3} className={classes.introText}>
-	      	<Grid item xs={4}>
-	      		Welcome
-	      	</Grid>
-	      	<Grid item xs={4}>
-	      		to
-	      	</Grid>
-	      	<Grid item xs={4}>
-	      		the
-	      	</Grid>
-	      </Grid>
-	      <div className={classes.titleText}>
-	        <div className={classes.daydream}>Daydream</div>
-	        <div>Exchange</div>
-	      </div>
+        <Grid container spacing={3} className={classes.introText}>
+          <Grid item xs={4} className={classes.welcome}>
+            Welcome
+          </Grid>
+          <Grid item xs={4} className={classes.to}>
+            to
+          </Grid>
+          <Grid item xs={4} className={classes.the}>
+            the
+          </Grid>
+        </Grid>
+        <div className={classes.titleText}>
+          <div className={classes.daydream}>Daydream</div>
+          <div>Exchange</div>
+        </div>
+        <div className={classes.tagline}>Write more, with others.</div>
       </div>
     </div>
   );
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 const smallScreen = window.screen.width < 650;
 
@@ -13,36 +13,38 @@ const useStyles = makeStyles((theme) => ({
   menu: {
     fontWeight: 600,
     textAlign: "center",
-    fontSize: '3vh',
+    fontSize: "2vh",
     fontFamily: "Cousine",
-    willChange: "auto",
-    // display: "block",
-    backgroundRepeat: "no-repeat",
-    // gradient from https://products.ls.graphics/mesh-gradients/
-    backgroundImage: "url(\"https://products.ls.graphics/mesh-gradients/images/05.-Flax.jpg\")",
-    backgroundSize: "200%",
-    backgroundClip: "text",
-    WebkitBackgroundClip: "text",
-    color: "transparent",
-    animation: "$bg 8s cubic-bezier(0.77, 0, 0.175, 1) 2s infinite",
   },
-  "@keyframes bg": {
-    "0%": {
-      backgroundPosition: "5% 5%",
+  menuItem: {
+    textDecoration: "none",
+    lineHeight: 1,
+    position: "relative",
+    zIndex: 0,
+    display: "inline-block",
+    padding: "5px 5px",
+    overflow: "hidden",
+    color: "#333",
+    verticalAlign: "bottom",
+    transition: "color .3s ease-out",
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      zIndex: -1,
+      top: 0,
+      left: 0,
+      transform: "translateY(calc(100% - 2px))",
+      width: "100%",
+      height: "100%",
+      backgroundImage: "linear-gradient(60deg, #f6cb62 0%, #ec4d11 100%)",
+      transition: "transform .25s ease-out",
     },
-    "25%": {
-      backgroundPosition: "5% 95%",
-      backgroundSize: "250%",
-    },
-    "50%": {
-      backgroundPosition: "95% 95%",
-      backgroundSize: "150%",
-    },
-    "75%": {
-      backgroundPosition: "5% 95%",
-    },
-    "100%": {
-      backgroundPosition: "5% 5%",
+    "&:hover": {
+      color: "white",
+      "&::before": {
+        transform: "translateY(0)",
+        transition: "transform .25s ease-out",
+      },
     },
   },
 }));
@@ -56,33 +58,22 @@ function ToolBar(props) {
 
   return (
     <Toolbar className={classes.root}>
-    <Grid container spacing={3} className={classes.menu}>
-        <Grid item xs={1}>
+      <Grid container spacing={3} className={classes.menu}>
+        <Grid item xs={1}></Grid>
+        <Grid item xs={2}>
+          <div className={classes.menuItem}>About</div>
         </Grid>
         <Grid item xs={2}>
-          {/*<Link to="/" style={{ textDecoration: 'none' }}>*/}
-            About
-          {/*</Link>*/}
+          <div className={classes.menuItem}>How it works</div>
+        </Grid>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={2}>
+          <div className={classes.menuItem}>Get Started</div>
         </Grid>
         <Grid item xs={2}>
-          {/*<Link to="/" style={{ textDecoration: 'none' }}>*/}
-            Contact
-          {/*</Link>*/}
+          <div className={classes.menuItem}>Login</div>
         </Grid>
-        <Grid item xs={2}>
-        </Grid>
-        <Grid item xs={2}>
-          {/*<Link to="/" style={{ textDecoration: 'none' }}>*/}
-            Contribute
-          {/*</Link>*/}
-        </Grid>
-        <Grid item xs={2}>
-          {/*<Link to="/" style={{ textDecoration: 'none' }}>*/}
-            Login
-          {/*</Link>*/}
-        </Grid>
-        <Grid item xs={1}>
-        </Grid>
+        <Grid item xs={1}></Grid>
       </Grid>
     </Toolbar>
   );
