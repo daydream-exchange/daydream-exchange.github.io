c6ab22eda4a7abca9ce1c9ae94ac7b086be96497
0xJeremy <jeremy.kanovsky@tufts.edu>
2021-05-18 01:12:10 -0400

    Updates

---
+++ precache-manifest.45db1ed28a7788ebc3609ceeb525e706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fb62dc89a7d1a94f7e135bfa6dc156",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d72ec2843a39c0508075",
+    "url": "./static/js/2.bbd039af.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "./static/js/2.bbd039af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffb3df3792f13a849ed",
+    "url": "./static/js/main.84089e16.chunk.js"
+  },
+  {
+    "revision": "eb2cb9f401df8054a48a",
+    "url": "./static/js/runtime-main.d5d33a6a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daydream-exchange.github.io/precache-manifest.db2a1220388090154b119c9fd8bec5ba.js"
+  "./precache-manifest.45db1ed28a7788ebc3609ceeb525e706.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daydream-exchange.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
